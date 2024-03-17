74b654eede091addb4fb5dda58d7e233b64a7efd
Muhammet <msolmaz83@gmail.com>
2024-03-17 02:55:31 +0100

    waitfor

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { getByText, render, screen, waitFor } from '@testing-library/react';
 //hangi sazfalari test edecegim
 import App from './App';
 
-test("app",()=>{
+test("app",async()=>{
   render(<App/>)
   // queryByText de yoklugunu kontrol edeceksin
   // getByText= varligini kontroledeceksin expect("elleme").not.toBe...
   //burda findBytext burda asyn kullaniriy app usettimey degisme durumu awit kullaniriy burasi onemli
   //https://testing-library.com/docs/react-testing-library/cheatsheet bu documant bakabiliry
- //const element = await screen.findByText("bbbb")
+   //const element = await screen.findByText("bbbb")
+   //burda findBzText yerine bunuda kullanabiliriy iceriyi bekliyor
+   let element
+   await waitFor(()=>{
+    element = screen.getByText("bbbb")
+   })
 //coklu buldurma
- const element = screen.getAllByText("dere")
+ //const element = screen.getAllByText("dere")
   //expect(element[0]).toBeInTheDocument();
-  expect(element).toHaveLength(2);
+  expect(element).toBeInTheDocument();
 })
 //bu elementlerin durunmu dikkqt edilmesi gerekir
 // No Match	1 Match	1+ Match	Await?
